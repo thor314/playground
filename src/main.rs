a004636da58a0d21369b0969b833fb9982ae044b
thor <thorck@protonmail.com>
2022-07-20 09:01:38 -0700

    init

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+//! A batteries-included binary template.
+
+// TODO: remove these when ready
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+#![allow(dead_code)]
+use anyhow::Result;
+use clap::Parser;
+use utils::MyError;
+use validator::{Validate, ValidationError};
+
+#[cfg(test)] mod tests;
+mod utils;
+fn main() -> Result<()> {
+  let context = utils::setup()?;
+  if std::env::var("DOTENV_OK").is_ok() {
+    log::info!("Hello, {}!", context.args.name);
+    #[cfg(feature = "some_feature")]
+    log::debug!("and build info: {:#?}", context.s);
+  }
+  Ok(())
+}
